7b3e4a7f0f0a113046ba293a16aec967ed760ad8
hazel <hazel@zipcodes-MacBook-Pro-97.local>
2021-03-12 13:55:47 -0500

    Started bins

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
 public class Bins {
+
+    private final int minimum;
+    private final int maximum;
+    ArrayList<Integer> bins;
+
+    public Bins(int minimum, int maximum){
+        this.minimum = minimum;
+        this.maximum = maximum;
+        this.bins = new ArrayList<Integer>(maximum - minimum + 1);
+    }
 
 }
